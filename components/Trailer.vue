ca557da39379a41f210ef1c66f01fdf10c542603
Mario Kaiser <ehmprah@gmail.com>
2021-12-03 12:46:45 +0100

    Update trailer

--- components/Trailer.vue
+++ components/Trailer.vue
@@ -4,7 +4,7 @@
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube-nocookie.com/embed/9cTpsxcMnAQ"
+        src="https://www.youtube-nocookie.com/embed/_AuOkjvMNPE"
         frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
